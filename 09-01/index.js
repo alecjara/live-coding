3607640302fe0a86025255c4e543e26a6405f08a
Hadi Nsreeny <hnsreeny@gmail.com>
2020-01-08 21:34:53 +0100

    new files

---
+++ 09-01/index.js
@@ -0,0 +1,31 @@
+// Addition
+// x + y * s
+
+// String + Number
+
+//concatenation
+// Number + String
+
+// String + Boolean
+
+// String + String
+
+//typeof  (Objects, functions, console... ) 😎
+
+//(0.1 * 0.2)
+
+// 1.23e+5
+
+// num.toFixed(num)
+// x = 3.444;
+// console.log(x.toFixed(2));
+
+// && ||
+
+// = Assigning
+
+// == vs ===
+
+// Truthy and falsy values: false, undefined, null, 0, ''
+
+//console.log(true ? "yeah" : "nope");
