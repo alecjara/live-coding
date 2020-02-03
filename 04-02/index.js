1c07dbff96ce47eaa8d108ad55bf5c4ffb00acf3
Hadi Nsreeny <hnsreeny@gmail.com>
2020-02-03 21:40:26 +0100

    Object

---
+++ 04-02/index.js
@@ -0,0 +1,16 @@
+// coming events
+
+// Objects
+// keys, properties : value
+
+// const <namespace> = { <key 1>: <value 1>, <key 2>: <value 2>, ... }
+
+// Accessing properties with bracket notation (no variables)
+
+// Accessing properties with dot notation
+
+// cool for 😎
+
+// Object.entries()
+
+// Methods
