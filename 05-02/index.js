41427f1b0be8a5004e17078b730d93ab55f0bb85
Hadi Nsreeny <hnsreeny@gmail.com>
2020-02-06 14:49:14 +0100

    zodiac

--- 05-02/index.js
+++ 05-02/index.js
@@ -70,9 +70,34 @@ function zodiac(name, day, month, year) {
     birthMonth: month,
     birthYear: year,
     yourZodiac: function() {
-      // do something
+      data = [
+        { signName: "Not real day!", from: "0000" },
+        { signName: "Capricorn", from: "0101" },
+        { signName: "Aquarius", from: "0121" },
+        { signName: "Pisces", from: "0220" },
+        { signName: "Aries", from: "0321" },
+        { signName: "Taurus", from: "0421" },
+        { signName: "Gemini", from: "0521" },
+        { signName: "Cancer", from: "0622" },
+        { signName: "Leo", from: "0723" },
+        { signName: "Virgo", from: "0824" },
+        { signName: "Libra", from: "0924" },
+        { signName: "Scorpio", from: "1024" },
+        { signName: "Sagittarius", from: "1123" },
+        { signName: "Capricorn", from: "1222" },
+        { signName: "Not real day!", from: "1232" },
+        { signName: "empty", from: "0000" }
+      ];
+      let monthDay = this.birthMonth + this.birthDay;
+      console.log(monthDay);
+      let i = 0;
+      while (monthDay >= data[i].from && i < data.length - 1) {
+        i++;
+      }
+      let yourZodiac = data[i - 1].signName;
+      return `Hey ${this.userName} your zodiac sign is ${yourZodiac}`;
     }
   };
   return result.yourZodiac();
 }
-zodiac("Hadi", "01", "09", "1989");
+console.log(zodiac("Hadi", "01", "09", "1989"));
