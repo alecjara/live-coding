e6c3fcbcce89396e52f1764063c1b5099176afc7
Hadi Nsreeny <hnsreeny@gmail.com>
2020-02-11 09:45:28 +0100

    nested array

--- 10-02/index.js
+++ 10-02/index.js
@@ -82,11 +82,11 @@ console.log(factorial(5));
 
 // Deep copy using recursion 🤨
 function deepCopy(obj) {
-  if (typeof obj == "object") {
+  if (typeof obj === "object") {
     return Object.keys(obj)
       .map(key => ({ [key]: deepCopy(obj[key]) }))
       .reduce((acc, cur) => Object.assign(acc, cur), {});
-  } else if (typeof obj == "array") {
+  } else if (typeof obj === "array") {
     return obj.map(deepCopy);
   }
   return obj;
@@ -95,18 +95,27 @@ const newObj = { 1: "hi", 2: "how are you", fun: () => console.log("Hi") };
 const superNewObj = deepCopy(newObj);
 console.log(superNewObj);
 // cool console 😎
-console.log();
-
+console.log(console);
+console.table(newObject);
 /* Check if a number is within a given range. Write a program that checks if a number is within the range of an object's min and max properties. Assume min <= max is always true.
 
 Examples:
 
-4, { min: 0, max: 5 }) ➞ true
-4, { min: 4, max: 5 }) ➞ true
-4, { min: 6, max: 10 }) ➞ false
-5, { min: 5, max: 5 }) ➞ true */
+(4, { min: 0, max: 5 }) ➞ true
+(4, { min: 4, max: 5 }) ➞ true
+(4, { min: 6, max: 10 }) ➞ false
+(5, { min: 5, max: 5 }) ➞ true */
+console.log("ex 1");
+let ojbRa = { min: 10, max: 11 };
+
+function fun(num, obj) {
+  return num >= obj.min && num <= obj.max;
+}
+console.log(fun(4, { min: 0, max: 5 }));
+console.log(fun(4, ojbRa));
 
 /*
+
 Scrabble. Write a program that, given an array of scrabble tiles, counts the maximum score that a player can earn from the tiles in their hand. Example:
  [ { tile: "N", score: 1 },
    { tile: "K", score: 5 }, 
@@ -118,7 +127,25 @@ Scrabble. Write a program that, given an array of scrabble tiles, counts the max
 
 The player's maximum score: 1 + 5 + 10 + 8 + 2 + 1 + 1 = 28 
 */
-
+console.log("ex 2");
+let arrayScore = [
+  { tile: "N", score: 1 },
+  { tile: "K", score: 5 },
+  { tile: "Z", score: 10 },
+  { tile: "X", score: 8 },
+  { tile: "D", score: 2 },
+  { tile: "A", score: 1 },
+  { tile: "E", score: 1 }
+];
+//console.log(arrayScore[3].score)
+function score(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += arrayScore[i].score;
+  }
+  return result;
+}
+console.log(score(arrayScore));
 /*
 checks it an empty object? Write a program that returns true if an object is empty, and false if otherwise.
 
@@ -127,7 +154,12 @@ Examples:
 {} ➞ true
 {a: 1} ➞ false 
 */
-
+console.log("ex 3");
+function isEmpty(obj) {
+  return Object.keys(obj).length == 0;
+}
+console.log(isEmpty({}));
+console.log(isEmpty({ 1: 333 }));
 /*
 Free Shipping. Create a function that determines whether an online order should get free shipping. An order gets free shipping if the total cost of items exceeds €50.
 Examples:
@@ -137,28 +169,69 @@ freeShipping({ "Surround Sound Equipment": 499.99 }) ➞ true
 freeShipping({ "Wool": 13.99, "Knitting Needles": 15.50, "Bag": 13.99 }) ➞ false 
 
 */
-
+console.log("ex 4");
+function freeShipping(obj) {
+  let values = Object.values(obj);
+  //console.log(values);
+  let result = 0;
+  for (let i = 0; i < values.length; i++) {
+    result += values[i];
+  }
+  return result > 50;
+}
+console.log(freeShipping({ Sponge: 3.5, Soap: 5.99 }));
+console.log(freeShipping({ "Surround Sound Equipment": 499.99 }));
 /*
 5. Programming Object.
-const programming = { languages: ["JavaScript", "Python", "Ruby"], isChallenging: true, isRewarding: true, difficulty: 8, jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke" }; Write the command to add the language "Go" to the end of the languages array.
+const programming = { languages: ["JavaScript", "Python", "Ruby"], isChallenging: true, isRewarding: true, difficulty: 8, jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke" }; 
 
-Change the difficulty to the value of 7.
-
-Using the delete keyword, write the command to remove the jokes key from the programming object.(need Google)
-
-Write a command to add a new key called isFun and a value of true to the programming object.
-
-Using a loop, iterate through the languages array and console.log all of the languages.
-
-Using a loop, console.log all of the keys in the programming object.
-
-Using a loop, console.log all of the values in the programming object.
-
-Create an object method where if the keys "isChallenging" and "isRewarding" have values of "true", then return "Learning the programming languages: "JavaScript, Python, Ruby, Go" is rewarding and challenging. Bonus: In a comment, explain what is printed if we console.log an object method without calling it and why.
-
-Bonus:
-
-Make sure that any other code cannot delete or change properties of the object.(need Google)
 */
+console.log("ex 5");
+const programming = {
+  languages: ["JavaScript", "Python", "Ruby"],
+  isChallenging: true,
+  isRewarding: true,
+  difficulty: 8,
+  jokes:
+    "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+};
+//Write the command to add the language "Go" to the end of the languages array.
+programming.languages.push("Go");
+// Change the difficulty to the value of 7.
+programming.difficulty = 7;
 
-// Object.getOwnPropertyNames
+// Using the delete keyword, write the command to remove the jokes key from the programming object.(need Google)
+delete programming.jokes;
+// Write a command to add a new key called isFun and a value of true to the programming object.
+programming.isFun = true;
+programming["isFun"] = true;
+// Using a loop, iterate through the languages array and console.log all of the languages.
+let array = programming.languages; //ref
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+// Using a loop, console.log all of the keys in the programming object.
+//let programmingKeys = Object.keys(programming);
+
+for (let key in programming) {
+  console.log(key);
+}
+// Using a loop, console.log all of the values in the programming object.
+for (let key in programming) {
+  console.log(programming[key]);
+}
+// Create an object method where if the keys "isChallenging" and "isRewarding" have values of "true", then return "Learning the programming languages: "JavaScript, Python, Ruby, Go" is rewarding and challenging.
+programming.isTrue = function() {
+  if (this.isChallenging && this.isRewarding) {
+    return `Learning the programming languages ${this.languages} is rewarding and challenging`;
+  } else {
+    return `Sorry something went wrong`;
+  }
+};
+console.log(programming.isTrue());
+// Bonus: In a comment, explain what is printed if we console.log an object method without calling it and why.
+
+// Bonus:
+
+// Make sure that any other code cannot delete or change properties of the object.(need Google)
+Object.freeze(programming);
--- 11-02/index.js
+++ 11-02/index.js
@@ -1,6 +1,40 @@
 // Nesting arrays
 // Nesting and accessing arrays within each other
-
+const arr = [
+  [2, 3, 4],
+  [533, 53],
+  [345, 56, 6]
+];
+// 111 222 333 444
+console.log(arr[1][1]);
+console.log(arr[2][1]);
+for (let i = 0; i < arr.length; i++) {
+  // o
+  for (let j = 0; j < arr[i].length; j++) {
+    // o
+    console.log(arr[i][j]); // 2 0
+  }
+}
+let arrayNew = [];
+for (let i = 0; i <= 5; i++) {
+  for (let j = 0; j <= 2; j++) {
+    arrayNew.push([i, j, i, 44]); // pushing an array
+  }
+}
+let names = ["Ali", "Olga", "Nancy"];
+names.forEach(name => console.log(name)); // just an ex
+names.map(name => console.log(name)); //
+let family = [
+  ["Ali", 22, "teacher"],
+  ["Olga", 40, "chef"]
+];
+for (let i = 0; i < family.length; i++) {
+  for (let j = 0; j < family[i].length; j++) {
+    console.log(family[i][j]);
+  }
+}
+family.forEach;
+// console.log(arrayNew);
 // Nesting objects
 // Nesting and accessing objects in objects
 
