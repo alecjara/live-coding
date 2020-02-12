89e602ce646ce8cacc45b1108a3bf92b155553ce
Hadi Nsreeny <hnsreeny@gmail.com>
2020-02-12 09:16:46 +0100

    Getter & Setter

--- 12-02/index.js
+++ 12-02/index.js
@@ -1,9 +1,32 @@
 // Destructuring objects as function parameters
+function fun(num, { name }) {
+  console.log(num);
+  let firstName = { name };
+  console.log(firstName);
+}
+fun(22, { name: "Ali" });
+const userData = {
+  firstName: "Hadi",
+  lastName: "Nsreeny",
+  get fullName() {
+    // this is getter method
+    console.log(`${this.firstName} ${this.lastName}`);
+  },
+  set fullName(str) {
+    // this is setter method
+    let arr = str.split(" ");
+    this.firstName = arr[0];
+    this.lastName = arr[1];
+  }
+};
 
 // getter
+userData.fullName;
+// userData.firstName = 'Ali';
+// userData.lastName = 'Xxx';
 
 // setter
-
+userData.fullName = "Ali xxx";
 // constructor()
 
 // class
