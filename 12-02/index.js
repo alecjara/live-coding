5d22d66c93a920e034bef08e5ffff1b077fdb9db
Hadi Nsreeny <hnsreeny@gmail.com>
2020-02-12 10:28:37 +0100

    Destructuring

--- 12-02/index.js
+++ 12-02/index.js
@@ -27,10 +27,44 @@ userData.fullName;
 
 // setter
 userData.fullName = "Ali xxx";
+userData.fullName; // getting new data
 // constructor()
 
+function Person(name, age, activities) {
+  // (this) referencing an empty object
+  // in js objects are dynamic so you can add property or change it
+  this.name = name;
+  this.age = age;
+  this.activities = activities;
+  this.printOut = function() {
+    console.log(`Hey ${this.name}, you are too old  ${this.age}`);
+  };
+}
+let ali = new Person("Ali", 22, ["party", "dance"]);
+let olga = new Person("Olga", 40, ["sleeping", "dance"]);
+console.log(ali);
+ali.printOut();
+const ojb = new Date();
+console.log(ojb);
 // class
-
+class Animals {
+  // setup
+  constructor(_kind, _hometown) {
+    this.kind = _kind;
+    this.hometown = _hometown;
+  }
+  // method
+  display() {
+    console.log(
+      `It is a very cute ${this.kind}, and lives in ${this.hometown} `
+    );
+  }
+}
+let casper = new Animals("Dog", "LA");
+casper.display();
+let sofi = new Animals("Fish", "Berlin");
+const dyson = new Animals("Fish", "Berlin");
+console.log(dyson);
 // extends  and super()
 // Array.filter()
 
