31741d7e3b006184382f9990b63073c5ee379e44
Hadi Nsreeny <hnsreeny@gmail.com>
2020-02-12 08:36:29 +0100

    Destructuring

--- 11-02/index.js
+++ 11-02/index.js
@@ -68,16 +68,37 @@ const tShirt = {
     console.log(this.color);
   }
 };
+// Nesting and accessing arrays in objects
 console.log(tShirt.size[2]);
 console.log(tShirt.countries.capital[1]);
 console.log(tShirt.countries.name[0]);
 console.log(tShirt.countries.obj.two);
-
-// Nesting and accessing arrays in objects
-
-// Nesting and iterating over objects in arrays
+tShirt.method(); // 1 inside t-shirt
+tShirt.countries.display(); // inside countries
+tShirt.countries.obj.printOut(); // obj
+let newShortWay = tShirt.countries;
+newShortWay.display();
 
 // Destructuring assignment
 // Extracting values from arrays: Array destructuring assignment
+let car = ["Ford", "pink", 2020, "enj", "xs"];
+// let carName = car[0];
+// let carColor = car[1];
+let [carName, carColor, carYear, ...rest] = car;
+console.log(carName);
+console.log(rest);
+console.log(car);
+// Nesting and iterating over objects in arrays
+const object = {
+  firstName: "Ali",
+  old: 22,
+  country: "Germany"
+};
 
 // Extracting values from objects: Object destructuring assignment
+
+({ firstName, old, country } = object);
+// tomorrow topic ({ firstName, old, country } = obj);
+console.log(firstName);
+
+//console.log(object.firstName);
---
+++ 12-02/index.js
@@ -0,0 +1,21 @@
+// Destructuring objects as function parameters
+
+// getter
+
+// setter
+
+// constructor()
+
+// class
+
+// extends  and super()
+// Array.filter()
+
+// Array.map()
+
+// Array.reduce()
+
+// Array.sort()
+
+// new Date()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/now
