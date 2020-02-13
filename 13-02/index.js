ffa3eb740e1d5544663d30b9e96a3e9387615943
Hadi Nsreeny <hnsreeny@gmail.com>
2020-02-13 09:23:10 +0100

    error

--- 13-02/index.js
+++ 13-02/index.js
@@ -7,11 +7,19 @@ if (userAge == 25) {
 }
 console.log("Hi");
 let userName = "";
+cool();
+// try --> to test a block of code
+// catch --> to handel the error
+// throw --> to create a custom errors
 try {
   console.log("Hi his is trying phase");
   // cool();
   if (userName == "") throw " Sorry something went wrong ";
+  // if there was an error this part will never run
   console.log("2nd phase");
 } catch (err) {
   console.log("this error is" + err);
+} finally {
+  userName = "xxx";
+  console.log("this will always run 😂");
 }
