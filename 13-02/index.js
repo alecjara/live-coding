60361ed044281fd80e54e44862a3ab52ea5baff3
Hadi Nsreeny <hnsreeny@gmail.com>
2020-02-13 09:17:38 +0100

    error

--- 13-02/index.js
+++ 13-02/index.js
@@ -1,7 +1,17 @@
-// 3 types of errors (compilation, runtime, logic)
+// 3 types of errors (compilation aka compiler fails to compile, runtime, logic)
 
 // `try { ... } catch (e) { ... }`
-
-// error
-
-//
+let userAge = 25;
+if (userAge == 25) {
+  console.log("something else");
+}
+console.log("Hi");
+let userName = "";
+try {
+  console.log("Hi his is trying phase");
+  // cool();
+  if (userName == "") throw " Sorry something went wrong ";
+  console.log("2nd phase");
+} catch (err) {
+  console.log("this error is" + err);
+}
