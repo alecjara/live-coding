e80cdec5fb655504ca34dd6e9a5fe9eb4d985b99
Hadi Nsreeny <hnsreeny@gmail.com>
2020-04-22 10:43:50 +0200

    DOM append

--- apr/22-04/index.js
+++ apr/22-04/index.js
@@ -8,3 +8,12 @@ const create = () => {
   newElement.appendChild(newText);
   document.body.appendChild(newElement);
 };
+const addToList = () => {
+  let userInput = document.querySelector("#userText").value;
+  console.log(userInput);
+  let newLi = document.createElement("li");
+  let text = document.createTextNode(userInput);
+  newLi.appendChild(text);
+  document.querySelector(".result").appendChild(newLi);
+  document.querySelector("#userText").value = "";
+};
