f649c8b4a9c662b41fb9389f2666d46c7bb1a906
Hadi Nsreeny <hnsreeny@gmail.com>
2020-05-05 15:12:45 +0200

    json

--- may/04-05/src/scripts/index.js
+++ may/04-05/src/scripts/index.js
@@ -14,33 +14,19 @@
 // It's a file format, and data interchange format, that uses human-readable text to store and transmit data objects
 
 import { data } from "./data.js";
-let parsedData = JSON.parse(data);
 
-const directorHtml = document.getElementById("director"),
-  imgHtml = document.querySelector("img"),
-  cardBody = document.querySelector(".card__body"),
-  toHover = document.querySelector(".toHover"),
-  section = document.querySelector(".list");
+let parsedData = JSON.parse(data);
+const section = document.querySelector("#bread");
+console.log(parsedData);
 
 parsedData.forEach((obj) => {
-  let { id, title } = obj;
-  let card = `<div id = ${id}>${title}</div>`;
+  console.log(obj);
+  let { id, title, ingredients, image } = obj;
+  let card = ` <div id=${id}>
+  <h2> ${title}</h2>
+  <img src=${image} alt=${title}>
+  <h6> ${ingredients}</h6>
+  </div>
+  `;
   section.innerHTML += card;
 });
-let listItems = document.querySelector(".list").querySelectorAll("div");
-listItems.forEach((item) => {
-  item.addEventListener("click", changes);
-});
-
-function changes() {
-  listItems.forEach((item) => {
-    this.style.backgroundColor = "#082e4d";
-    this.style.transform = "translateX(2rem)";
-    item.removeAttribute("style");
-    toHover.style.opacity = "1";
-  });
-  let data = parsedData.find((data) => data.id == this.id);
-  directorHtml.innerHTML = data.director;
-  cardBody.innerHTML = data.description;
-  imgHtml.src = data.image;
-}
