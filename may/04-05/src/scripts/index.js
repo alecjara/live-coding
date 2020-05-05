e03e2ae5fa3f60a3ebbb6b569529ce4c383125b2
Hadi Nsreeny <hnsreeny@gmail.com>
2020-05-05 09:55:39 +0200

    Promise

--- may/04-05/src/scripts/index.js
+++ may/04-05/src/scripts/index.js
@@ -15,17 +15,32 @@
 
 import { data } from "./data.js";
 let parsedData = JSON.parse(data);
-console.log(parsedData);
-const section = document.querySelector("#bread");
+
+const directorHtml = document.getElementById("director"),
+  imgHtml = document.querySelector("img"),
+  cardBody = document.querySelector(".card__body"),
+  toHover = document.querySelector(".toHover"),
+  section = document.querySelector(".list");
+
 parsedData.forEach((obj) => {
-  console.log(obj);
-  let { id, title, ingredients, image } = obj;
-  //console.log(id);
-  let card = ` <div id=${id}>
-  <h2> ${title} </h2>
-  <img src=${image} alt=${title}>
-  <h6> ${ingredients} </h6>
-  </div>
-  `;
+  let { id, title } = obj;
+  let card = `<div id = ${id}>${title}</div>`;
   section.innerHTML += card;
 });
+let listItems = document.querySelector(".list").querySelectorAll("div");
+listItems.forEach((item) => {
+  item.addEventListener("click", changes);
+});
+
+function changes() {
+  listItems.forEach((item) => {
+    this.style.backgroundColor = "#082e4d";
+    this.style.transform = "translateX(2rem)";
+    item.removeAttribute("style");
+    toHover.style.opacity = "1";
+  });
+  let data = parsedData.find((data) => data.id == this.id);
+  directorHtml.innerHTML = data.director;
+  cardBody.innerHTML = data.description;
+  imgHtml.src = data.image;
+}
