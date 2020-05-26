ae78ae4c81acedbb186883149d9704f94ee2a385
Hadi Nsreeny <hnsreeny@gmail.com>
2020-05-26 09:34:04 +0200

    file sys

--- may/26-05/index.js
+++ may/26-05/index.js
@@ -9,8 +9,27 @@ function parseNum(arg) {
   }
   return number;
 }
-
+function sum(numArray) {
+  return numArray.reduce((sum, n) => sum + n, 0);
+}
+function avg(numArray) {
+  return sum(numArray) / numArray.length;
+}
 const [operation, ...rest] = args;
 
 const numbers = rest.map(parseNum);
 // sum 3 5 6 kdfjb
+// avg 45 6 67
+
+switch (operation) {
+  case "sum":
+    console.log(sum(numbers));
+    break;
+  case "avg":
+    console.log(avg(numbers));
+    break;
+  default:
+    console.log(
+      "I cannot do this, please type 'sum' to calculate Sum or 'avg' to calculate Avg"
+    );
+}
