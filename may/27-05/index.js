de59dc123d2ef806bfe0e6878b75f4414b1c4453
Hadi Nsreeny <hnsreeny@gmail.com>
2020-05-27 09:11:38 +0200

    moudle exports

--- may/27-05/index.js
+++ may/27-05/index.js
@@ -1,3 +1,10 @@
 // module exports
 
-const math = require();
+const math = require("./math");
+console.log(math);
+
+let x = 5,
+  y = 4;
+console.log(`x + y = ${math.sum(x, y)}`);
+console.log(`x + y = ${math.sub(x, y)}`);
+console.log(`x + y = ${math.multi(x, y)}`);
