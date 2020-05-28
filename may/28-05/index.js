c8acc63b1911945b7466f8eab191b533198b2cbc
Hadi Nsreeny <hnsreeny@gmail.com>
2020-05-28 09:21:25 +0200

    more args

--- may/28-05/index.js
+++ may/28-05/index.js
@@ -1,1 +1,11 @@
-
+//const { sum } = require("./script");
+const { prepareString } = require("./script");
+const { showHelp } = require("./messaging");
+const args = process.argv.slice(2);
+// let x = parseFloat(args[0]);
+// let y = parseFloat(args[1]);
+// console.log(`x + y = ${sum(x, y)}`);
+// console.log(`x - y = ${sub(x, y)}`);
+if (args.includes("--help")) {
+  showHelp();
+}
--- may/28-05/script.js
+++ may/28-05/script.js
@@ -1,1 +1,5 @@
+exports.sum = (x, y) => x + y;
+exports.sub = (x, y) => x - y;
 
+exports.multi = (x, y) => x * y;
+exports.prepareString = (str) => {};
